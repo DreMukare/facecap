64553d754cd38a0b821e430cf9ccbe43eeb61aac
wmik <williemik.wmik@gmail.com>
2019-03-28 22:28:34 +0300

    refactor: use knex migrations

---
+++ migrations/20190328220220_create_schema.js
@@ -0,0 +1,51 @@
+exports.up = function(knex, Promise) {
+  return knex.schema
+    .createTable('authentication', function(table) {
+      table.increments().primary();
+      table.string('login_id', 15).notNullable();
+      table.text('password').notNullable();
+      table.timestamps(false, true);
+    })
+    .createTable('courses', function(table) {
+      table.increments('course_id').primary();
+      table.string('course_code', 5);
+      table.string('course_name', 30);
+      table.timestamps(false, true);
+    })
+    .createTable('units', function(table) {
+      table.increments('unit_id').primary();
+      table.string('unit_code', 10);
+      table.string('unit_name', 30);
+      table.timestamps(false, true);
+      table
+        .integer('course_id')
+        .unsigned()
+        .references('course_id')
+        .inTable('courses')
+        .notNull()
+        .onDelete('cascade')
+        .index();
+    })
+    .createTable('students', function(table) {
+      table.increments('student_id').primary();
+      table.string('first_name', 10);
+      table.string('last_name', 10);
+      table.timestamps(false, true);
+      table
+        .integer('course_id')
+        .unsigned()
+        .references('course_id')
+        .inTable('courses')
+        .notNull()
+        .onDelete('cascade')
+        .index();
+    });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema
+    .dropTableIfExists('authentication')
+    .dropTableIfExists('courses')
+    .dropTableIfExists('units')
+    .dropTableIfExists('students');
+};
